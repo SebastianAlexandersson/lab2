f40e2379de56562ed088a631316c83a11c5f2612
Sebastian Alexandersson <fortheloveofgood@gmail.com>
2019-09-03 10:34:41 +0200

    ändra färg, nya bilder

--- script.js
+++
@@ -1,7 +0,0 @@
-const inputs = document.querySelectorAll(".form-container form input")
-
-document.getElementById("registration-form")
-  .addEventListener("submit", e => {
-    e.preventDefault()
-    inputs.forEach(input => input.type != "submit" ? input.value = "" : null)
-  });
